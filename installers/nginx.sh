a915c4b0071396c9944ca9aaa9f3b3e3d58e8200
Chris Turner <chris@turnerlogic.com>
2014-09-15 08:55:01 -0500

    commented features.

--- installers/nginx.sh
+++ installers/nginx.sh
@@ -21,10 +21,10 @@ apt-get -y install nginx
 mkdir -p /var/log/nginx
 
 # Copy nginx config template.
-cp $INSTALLERS_DIR/../templates/nginx/nginx.conf /etc/nginx/nginx.conf
+#cp $INSTALLERS_DIR/../templates/nginx/nginx.conf /etc/nginx/nginx.conf
 
 # Create a location for nginx configuration includes and copy include templates.
-mkdir -p /etc/nginx/includes
-cp $INSTALLERS_DIR/../templates/nginx/location.conf /etc/nginx/includes/location.conf
+#mkdir -p /etc/nginx/includes
+#cp $INSTALLERS_DIR/../templates/nginx/location.conf /etc/nginx/includes/location.conf
 
 touch /tmp/restart-nginx
