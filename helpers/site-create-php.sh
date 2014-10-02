6093d8bf3820d6f84f44d01fc50b5057d905d05d
Chris Turner <chris@turnerlogic.com>
2014-10-02 18:45:32 -0500

    Fixed env setting issue.

--- helpers/site-create-php.sh
+++ helpers/site-create-php.sh
@@ -60,7 +60,7 @@ if [ $USER != "vagrant" ]; then
 
 else
 
-	echo "env[LARAVEL_ENV] = local" | sudo tee -a /etc/php5/fpm/pool.d/php.dev.conf
+	echo "env[LARAVEL_ENV] = local" | sudo tee -a /etc/php5/fpm/pool.d/$1.conf
 
 fi
 
