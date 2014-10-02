6853b49096c03d973f212f802d213a382bbbbbbb
Chris Turner <chris@turnerlogic.com>
2014-10-02 15:06:08 -0500

    More ruby fixes.

--- installers/ruby.sh
+++ installers/ruby.sh
@@ -8,15 +8,15 @@ fi
 
 apt-get -y install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties
 
-su $USER
+cd /usr/local
+git clone git://github.com/sstephenson/rbenv.git rbenv
+git clone git://github.com/sstephenson/ruby-build.git rbenv/plugins/ruby-build
 
-git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
-git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-echo 'export PATH="$HOME/.rbenv/bin:$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bash_profile
+echo 'export PATH="/usr/local/rbenv/bin:/usr/local/rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bash_profile
 echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
 exec $SHELL
 
 rbenv install 2.1.3
 rbenv global 2.1.3
 
-exit
+chown -R $USER:$USER rbenv
--- templates/bash/.bash_profile
+++ templates/bash/.bash_profile
@@ -3,6 +3,12 @@
 # Add helpers directory to the path.
 export PATH="$HOME/warpspeed/helpers:$PATH"
 
+# Add rbenv to the path.
+export PATH="/usr/local/rbenv/bin:/usr/local/rbenv/plugins/ruby-build/bin:$PATH"
+
+# Initialize rbenv.
+eval "$(rbenv init -)"
+
 # If the vagrant home directory exists, assume we are using vagrant.
 if [ -d "/home/vagrant" ]; then
 	SYSTEM_ENV="local"
