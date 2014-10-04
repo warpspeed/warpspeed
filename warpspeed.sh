5ef405bb6fc88d4c326f81bb245d290235b9212f
Chris Turner <chris@turnerlogic.com>
2014-10-04 14:56:49 -0500

    Added argument handling.

--- warpspeed.sh
+++ warpspeed.sh
@@ -5,26 +5,56 @@ if [ $(id -u) != "0" ]; then
     exit 1
 fi
 
+# Declare array to track installers that should be run.
+# Installers are added via command line args by passing --installer.
+INSTALLERS=()
+
+# Retrieve system ip address.
+IPADDRESS=$(ifconfig eth0 | awk -F: '/inet addr:/ {print $2}' | awk '{ print $1 }')
+
+# Determine the directory this script is executing from.
+SCRIPTS_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 ###############################################################################
-# Input parameters.
+# Process command line arguments and make sure required args were provided.
 ###############################################################################
 
-# Hostname of the server (required).
-HOSTNAME=$1
+for arg in "$@"; do
+case $arg in
+    -h=*|--hostname=*)
+        HOSTNAME="${arg#*=}"
+        shift
+    ;;
+    -k=*|--sshkey=*)
+        SSHKEY="${arg#*=}"
+        shift
+    ;;
+    -p=*|--password=*)
+        PASSWORD="${arg#*=}"
+        shift
+    ;;
+    --*)
+        # Add arg to installers array.
+        INSTALLERS+=("${arg:2}")
+        shift
+    ;;
+esac; done
 
-# SSH public key for authentication (required).
-SSHKEY=$2
-
-# Password for warpspeed user (optional, defaults to system generated).
-PASSWORD=$3
-
-if [ -z "$HOSTNAME" ] || [ -z "$SSHKEY" ]; then
-  echo "Usage: init-server.sh hostname sshkey [password]" 1>&2
+if [ -z "$HOSTNAME" ] || [ -z "$SSHKEY" ] || [ -z "$PASSWORD" ]; then
+  echo "Usage: warpspeed.sh [OPTION]..." 1>&2
+  echo "Initializes a server and runs any installer scripts specified in the options." 1>&2
+  echo "For complete information, visit: warpspeed.io" 1>&2
+  echo -en "\n" 1>&2
+  echo "Mandatory arguments:" 1>&2
+  echo "  -h, --hostname=HOSTNAME         Hostname to be used for server." 1>&2
+  echo "  -k, --sshkey='SSH PUBLIC KEY'   Public key used for authentication to server." 1>&2
+  echo "  -p, --password=PASSWORD         Password for the warpspeed user (and database admins)." 1>&2
+  echo -en "\n" 1>&2
+  echo "Optional arguments:" 1>&2
+  echo "  --installer                     Installer script to run. Ex: --php will run the 'php.sh'" 1>&2
+  echo "                                  installer script found in the installers directory." 1>&2
+  echo -en "\n" 1>&2
   exit 1
-fi
-
-if [ -z "$PASSWORD" ]; then
-    PASSWORD=`tr -cd '[:alnum:]' < /dev/urandom | fold -w20 | head -n1`
 fi
 
 ###############################################################################
@@ -42,7 +72,7 @@ sed -i "s/^127\.0\.1\.1.*/127\.0\.1\.1\t$HOSTNAME $HOSTNAME/" /etc/hosts
 ln -s -f /usr/share/zoneinfo/UTC /etc/localtime
 
 ###############################################################################
-# Run system updates, and add new ppas.
+# Run system updates and install prerequisites.
 ###############################################################################
 
 apt-get update
@@ -125,6 +155,26 @@ chmod 0700 /home/warpspeed/.ssh
 chmod 0600 /home/warpspeed/.ssh/authorized_keys
 
 ###############################################################################
+# Setup bash profile.
+###############################################################################
+
+cp -f $SCRIPTS_ROOT/templates/bash/.bash_profile /home/warpspeed/.bash_profile
+chown warpspeed:warpspeed /home/warpspeed/.bash_profile
+
+###############################################################################
+# Run all installers that were passed as arguments.
+###############################################################################
+
+for installer in "${INSTALLERS[@]}"; do
+	INSTALLER_FULL_PATH="$SCRIPTS_ROOT/installers/$installer.sh"
+	if [ -x "$INSTALLER_FULL_PATH" ]; then
+		# Installer exists and is executable, run it.
+		# Note: Installer scripts will have access to vars declared herein.
+		source "$INSTALLER_FULL_PATH"
+	fi
+done
+
+###############################################################################
 # Restart services and show summary info.
 ###############################################################################
 
@@ -134,13 +184,8 @@ for service_name in $(ls /tmp/ | grep restart-* | cut -d- -f2-10); do
     rm -f /tmp/restart-$service_name
 done
 
-# Retrieve system ip address for display purposes.
-IPADDRESS=$(ifconfig eth0 | awk -F: '/inet addr:/ {print $2}' | awk '{ print $1 }')
-
 echo "Server initialization complete."
 echo "User: warpspeed was created with password: $PASSWORD"
 echo "Please record this information and keep it in a safe place."
 echo "To remotely access this server, use the following command:"
 echo "ssh warpspeed@$IPADDRESS"
-
-exit 0
---
+++ warspeed-vagrant.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+if [ $(id -u) != "0" ]; then
+    echo "This script must be run as root." 1>&2
+    exit 1
+fi
+
+# Declare array to track installers that should be run.
+# Installers are added via command line args by passing --installer.
+INSTALLERS=()
+
+# Retrieve system ip address.
+IPADDRESS=$(ifconfig eth0 | awk -F: '/inet addr:/ {print $2}' | awk '{ print $1 }')
+
+# Determine the directory this script is executing from.
+SCRIPTS_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+###############################################################################
+# Process command line arguments.
+###############################################################################
+
+for arg in "$@"; do
+case $arg in
+	-h=*|--hostname=*)
+        HOSTNAME="${arg#*=}"
+        shift
+    ;;
+    --*)
+        # Add arg to installers array.
+        INSTALLERS+=("${arg:2}")
+        shift
+    ;;
+esac; done
+
+if [ -z "$HOSTNAME" ]; then
+  echo "Usage: warpspeed-vagrant.sh [OPTION]..." 1>&2
+  echo "Initializes a vagrant server and runs any installer scripts specified in the options." 1>&2
+  echo "For complete information, visit: warpspeed.io" 1>&2
+  echo -en "\n" 1>&2
+  echo "Mandatory arguments:" 1>&2
+  echo "  -h, --hostname=HOSTNAME         Hostname to be used for server." 1>&2
+  echo -en "\n" 1>&2
+  echo "Optional arguments:" 1>&2
+  echo "  --installer                     Installer script to run. Ex: --php will run the 'php.sh'" 1>&2
+  echo "                                  installer script found in the installers directory." 1>&2
+  echo -en "\n" 1>&2
+  exit 1
+fi
+
+###############################################################################
+# Update system hostname and add to hosts file.
+###############################################################################
+
+echo $HOSTNAME > /etc/hostname
+hostname -F /etc/hostname
+sed -i "s/^127\.0\.1\.1.*/127\.0\.1\.1\t$HOSTNAME $HOSTNAME/" /etc/hosts
+
+###############################################################################
+# Set timezone to UTC
+###############################################################################
+
+ln -s -f /usr/share/zoneinfo/UTC /etc/localtime
+
+###############################################################################
+# Run system updates and install prerequisites.
+###############################################################################
+
+apt-get update
+apt-get -y upgrade
+apt-get -y install python-software-properties build-essential git-core
+
+###############################################################################
+# Setup bash profile.
+###############################################################################
+
+cp -f $SCRIPTS_ROOT/templates/bash/.bash_profile /home/vagrant/.bash_profile
+chown vagrant:vagrant /home/vagrant/.bash_profile
+
+###############################################################################
+# Run all installers that were passed as arguments.
+###############################################################################
+
+for installer in "${INSTALLERS[@]}"; do
+	INSTALLER_FULL_PATH="$SCRIPTS_ROOT/installers/$installer.sh"
+	if [ -x "$INSTALLER_FULL_PATH" ]; then
+		# Installer exists and is executable, run it.
+		# Note: Installer scripts will have access to vars declared herein.
+		source "$INSTALLER_FULL_PATH"
+	fi
+done
+
+###############################################################################
+# Clean up and restart services.
+###############################################################################
+
+# Restarts services that have a restart-service_name file in /tmp.
+for service_name in $(ls /tmp/ | grep restart-* | cut -d- -f2-10); do
+    service $service_name restart
+    rm -f /tmp/restart-$service_name
+done
