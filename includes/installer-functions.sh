7dafd68a81d517fa279e1ba1024c372fea7309c9
Chris Turner <chris@turnerlogic.com>
2015-02-01 22:17:57 -0600

    Added additional common packages.

--- includes/installer-functions.sh
+++ includes/installer-functions.sh
@@ -40,7 +40,7 @@ ws_run_system_updates() {
 }
 
 ws_setup_common_packages() {
-    apt-get -y install python-software-properties build-essential git-core supervisor
+    apt-get -y install python-software-properties build-essential git-core supervisor curl zlib1g-dev libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev
 }
 
 ws_create_user() {
