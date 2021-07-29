abfcf792576b99820b0281c38de1a652506f347f
Cory Rodriguez <coreyreylp@gmail.com>
2021-07-29 11:53:59 -0500

    Change go download verbosity

--- installers/go.sh
+++ installers/go.sh
@@ -16,7 +16,7 @@ ws_require_root
 ws_log_header "Installing go."
 
 # Download go binaries.
-wget -c https://dl.google.com/go/go1.16.6.linux-amd64.tar.gz
+wget -c -nv https://dl.google.com/go/go1.16.6.linux-amd64.tar.gz
 
 # Extract files, set permissions, and move to proper location.
 tar -C /usr/local -xzf go1.16.6.linux-amd64.tar.gz
@@ -26,7 +26,7 @@ echo '# Go configuration.' >> /home/$WARPSPEED_USER/.bashrc
 echo 'export GOROOT=/usr/local/go' >> ~/.bashrc
 echo 'export GOPATH=$WARPSPEED_ROOT/sites' >> ~/.bashrc
 echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.bashrc
-echo -en "\n" >>  /home/$WARPSPEED_USER/.bashrc
+echo -en "\n" >> /home/$WARPSPEED_USER/.bashrc
 
 # Clean up.
 rm go1.16.6.linux-amd64.tar.gz
