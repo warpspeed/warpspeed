0913493457c43e6979ffc75f6123d73bcdf16c40
Cory Rodriguez <coreyreylp@gmail.com>
2021-08-05 11:06:23 -0500

    Update php version to 7.4

--- installers/php.sh
+++ installers/php.sh
@@ -13,20 +13,17 @@ source $WARPSPEED_ROOT/includes/installer-functions.sh
 # Require that the root user be executing this script.
 ws_require_root
 
-ws_log_header "Installing php8.0."
+ws_log_header "Installing php7.4."
 
-LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php
-apt-get update
-
-apt-get -y install php8.0 php8.0-cli php8.0-curl php8.0-dev php8.0-fpm php8.0-gd php8.0-imagick php8.0-imap php8.0-intl php8.0-mbstring php8.0-memcached php8.0-mongodb php8.0-mysql php8.0-pgsql php8.0-zip
+apt-get -y install php7.4 php7.4-cli php7.4-curl php7.4-dev php7.4-fpm php7.4-gd php7.4-imagick php7.4-imap php7.4-intl php7.4-mbstring php7.4-memcached php7.4-mongodb php7.4-mysql php7.4-pgsql php7.4-zip
 
 # Install debug tools only for vagrant environment.
 if [ $WARPSPEED_USER == "vagrant" ]; then
-    apt-get -y install php8.0-xdebug
+    apt-get -y install php7.4-xdebug
 fi
 
 # Remove the default php-fpm pool.
-mv -f /etc/php/8.0/fpm/pool.d/www.conf /etc/php/8.0/fpm/pool.d/www.conf.orig
+mv -f /etc/php/7.4/fpm/pool.d/www.conf /etc/php/7.4/fpm/pool.d/www.conf.orig
 
 # Create directory for logging.
 mkdir -p /var/log/php
@@ -43,7 +40,7 @@ apt-get -y install libmcrypt-dev
 yes '' | pecl install mcrypt-1.0.4
 
 # Backup original and then modify php ini settings for fpm.
-PHPINI=/etc/php/8.0/fpm/php.ini
+PHPINI=/etc/php/7.4/fpm/php.ini
 cp $PHPINI $PHPINI.orig
 sed -i 's/^display_errors = On/display_errors = Off/' $PHPINI
 sed -i 's/^expose_php = On/expose_php = Off/' $PHPINI
@@ -52,7 +49,7 @@ sed -i 's/^;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/' $PHPINI
 echo 'extension=mcrypt.so' >> $PHPINI
 
 # Backup original and then modify php ini settings for cli.
-PHPINI=/etc/php/8.0/cli/php.ini
+PHPINI=/etc/php/7.4/cli/php.ini
 cp $PHPINI $PHPINI.orig
 sed -i 's/^;date.timezone =.*/date.timezone = UTC/' $PHPINI
 sed -i 's@;error_log =.*@error_log = /var/log/php/error-cli.log@' $PHPINI
@@ -63,5 +60,5 @@ curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
 
 # Stop the service and remove startup files.
-service php8.0-fpm stop
-systemctl disable php8.0-fpm
+service php7.4-fpm stop
+systemctl disable php7.4-fpm
