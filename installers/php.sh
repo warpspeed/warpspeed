a9e171843440b9d46ded66a4c08e7a493867d4f7
Chris Turner <chris@turnerlogic.com>
2023-03-09 12:56:25 -0600

    Updated to PHP 8.2.

--- installers/php.sh
+++ installers/php.sh
@@ -13,17 +13,26 @@ source $WARPSPEED_ROOT/includes/installer-functions.sh
 # Require that the root user be executing this script.
 ws_require_root
 
-ws_log_header "Installing php7.4."
+ws_log_header "Installing php8.2."
 
-apt-get -y install php7.4 php7.4-cli php7.4-curl php7.4-dev php7.4-fpm php7.4-gd php7.4-imagick php7.4-imap php7.4-intl php7.4-mbstring php7.4-memcached php7.4-mongodb php7.4-mysql php7.4-pgsql php7.4-zip
+
+apt update
+apt install -y ca-certificates apt-transport-https software-properties-common
+
+add-apt-repository -y ppa:ondrej/php
+
+
+apt install -y php8.2 php8.2-cli php8.2-curl php8.2-dev php8.2-fpm php8.2-gd php8.2-imagick php8.2-imap php8.2-intl php8.2-mbstring php8.2-memcached php8.2-mongodb php8.2-mysql php8.2-pgsql php8.2-zip
+
+sed -i "/#\$nrconf{restart} = 'i';/s/.*/\$nrconf{restart} = 'a';/" /etc/needrestart/needrestart.conf
 
 # Install debug tools only for vagrant environment.
 if [ $WARPSPEED_USER == "vagrant" ]; then
-    apt-get -y install php7.4-xdebug
+    apt-get -y install php8.2-xdebug
 fi
 
 # Remove the default php-fpm pool.
-mv -f /etc/php/7.4/fpm/pool.d/www.conf /etc/php/7.4/fpm/pool.d/www.conf.orig
+mv -f /etc/php/8.2/fpm/pool.d/www.conf /etc/php/8.2/fpm/pool.d/www.conf.orig
 
 # Create directory for logging.
 mkdir -p /var/log/php
@@ -34,7 +43,7 @@ mkdir -p /var/lib/php
 chown -R $WARPSPEED_USER:www-data /var/lib/php
 
 # Backup original and then modify php ini settings for fpm.
-PHPINI=/etc/php/7.4/fpm/php.ini
+PHPINI=/etc/php/8.2/fpm/php.ini
 cp $PHPINI $PHPINI.orig
 sed -i 's/^display_errors = On/display_errors = Off/' $PHPINI
 sed -i 's/^expose_php = On/expose_php = Off/' $PHPINI
@@ -42,7 +51,7 @@ sed -i 's/^;date.timezone =.*/date.timezone = UTC/' $PHPINI
 sed -i 's/^;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/' $PHPINI
 
 # Backup original and then modify php ini settings for cli.
-PHPINI=/etc/php/7.4/cli/php.ini
+PHPINI=/etc/php/8.2/cli/php.ini
 cp $PHPINI $PHPINI.orig
 sed -i 's/^;date.timezone =.*/date.timezone = UTC/' $PHPINI
 sed -i 's@;error_log =.*@error_log = /var/log/php/error-cli.log@' $PHPINI
@@ -52,5 +61,5 @@ curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
 
 # Stop the service and remove startup files.
-service php7.4-fpm stop
-systemctl disable php7.4-fpm
+service php8.2-fpm stop
+systemctl disable php8.2-fpm
